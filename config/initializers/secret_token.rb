1c58fbecfd8dc3e51d35b644fd03e08f6633375b
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-07-02 16:15:01 -0700

    basic rail structure instantiated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubymatch::Application.config.secret_token = '61f444936a82b71e957e44b84e72e083c98ad137968280e0c41bc4f8f283e436854c63f3266ae0041c7524596153a7f151c9908df6167799ac929ea126725367'
